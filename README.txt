aa5fde1ce14e47349684c679d5983f854af2069a
momo1478 <monish1478@gmail.com>
2019-01-27 23:44:25 -0800

    Better Graph, Got Default Emote List

---
+++ README.txt
@@ -0,0 +1,4 @@
+Small 1 day project to graph chat activity in a twitch channel given a VOD id
+(which can be found in the url of a vod). Sample graph provided. There's plan
+s to extend this to automatically generate clips and perform analysis on vari
+ous emotes that people use in the channel. Hope you enjoy!
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import requests
-import json
 from Comment import Comment
 
+import requests
+import json
+
+from pprint import pprint
+import os.path
+
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
+
+import time
 
 clientID = "chl2lhgpv1bjhmzwsjy9iuxpv5zejd"
 vodID = "354278744"
@@ -10,7 +18,7 @@ end = 'eyJpZCI6ImE2ZjkxZDJlLWZmNzYtNDNiOS1hZGY3LTFlZGQ4YTI1YTExYiIsImhrIjoiYnJvY
 
 windowTime = 30 #seconds
 
-def get(path , params):
+def get(path : str, params : dict = {}):
     headers = { "Client-ID" : clientID, "Accept" : "application/vnd.twitchtv.v5+json" }
     response = requests.get(path,headers=headers,params=params)
 
@@ -18,6 +26,13 @@ def get(path , params):
         print("Non 200 status code recieved")
     
     return response
+
+def get_emotes_list(setNumber : int = 0):
+    url = "https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=%d" % setNumber
+    emoteList = get(url).json()['emoticon_sets']['0']
+    ids =   [emote['id'] for emote in emoteList]
+    names = [emote['code'] for emote in emoteList]
+    return dict(zip(ids, names))
 
 def get_chat_fragment(vodID : str, cursor : str = ''):
     url = "https://api.twitch.tv/kraken/videos/%s/comments" % vodID
@@ -33,18 +48,18 @@ def download_chat_to_file(vodID : str):
             comments.append(Comment(comment))
         print("downloaded up to " + str(fragment['comments'][0]['content_offset_seconds']) + " seconds")
 
-    with open('VOD#' + vodID, 'w') as outfile:
+    with open('VOD#' + str(vodID), 'w') as outfile:
         json.dump(comments, outfile,default=Comment.toJSON,indent=4)
     
-    perform_analytics(comments)
+    perform_analytics(comments, vodID)
 
 def read_chat_from_file(vodID : str):
     comments = None
-    with open('VOD#' + vodID, 'r') as comment_data:
+    with open('VOD#' + str(vodID), 'r') as comment_data:
         comments = json.load(comment_data, object_hook=Comment.fromJSON)
-    perform_analytics(comments)
-    
-def perform_analytics(comments : list):
+    perform_analytics(comments, vodID)
+
+def perform_analytics(comments : list, vodID : str):
     window = []
     commentsInWindow = {}
     for comment in comments:
@@ -53,15 +68,35 @@ def perform_analytics(comments : list):
         window = [time for time in window if newTime - time < windowTime]
         commentsInWindow[newTime] = len(window)
     
-    plot_data(commentsInWindow)
+    plot_data(commentsInWindow, vodID)
 
-def plot_data(data : dict):
+def plot_data(data : dict, vodID : str):
     x = list(data.keys())
     y = list(data.values())
 
+    fig, ax = plt.subplots()
+
     plt.plot(x, y)
+
+    formatter = mticker.FuncFormatter(lambda hms, x: time.strftime('%H:%M:%S', time.gmtime(hms)))
+    ax.xaxis.set_major_formatter(formatter)
+
+    numComments = len(data)
+    lastComment = time.strftime('%H:%M:%S', time.gmtime(x[-1]))
+
+    plt.title("VOD#" + str(vodID) + (" (%d comments in %s )" % (numComments,lastComment) ) )
+    plt.xlabel("Time into VOD")
+    plt.ylabel("Comments in " + str(windowTime) + " second window.")
 
     plt.show()
 
-download_chat_to_file(vodID)
-#read_chat_from_file(vodID)
+def analyze_VOD(vodID):
+    if os.path.isfile('VOD#' + str(vodID)):
+        read_chat_from_file(vodID)
+    else:
+        download_chat_to_file(vodID)
+
+# emotes = get_emotes_list()
+# pprint(emotes)
+# analyze_VOD(vodID)
+analyze_VOD(371095364)
